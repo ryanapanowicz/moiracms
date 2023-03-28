5152a92eb687db4cc36c443646167baaf2e01130
Ryan Apanowicz <ryan.apanowicz@cgphoenix.com>
2023-03-27 22:36:04 -0400

    Fix permissions migration model_morph_key type

--- database/migrations/2021_06_09_203343_create_permission_tables.php
+++ database/migrations/2021_06_09_203343_create_permission_tables.php
@@ -42,7 +42,7 @@ class CreatePermissionTables extends Migration
             $table->uuid('permission_id');
 
             $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
             $table->foreign('permission_id')
@@ -58,7 +58,7 @@ class CreatePermissionTables extends Migration
             $table->uuid('role_id');
 
             $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
 
             $table->foreign('role_id')
