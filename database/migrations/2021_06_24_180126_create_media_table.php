a7ff88b1c69c32d509c83d4bd93f86d43733041f
Ryan Apanowicz <ryan.apanowicz@cgphoenix.com>
2023-03-28 21:50:55 -0400

    Fix media table morph type to uuid

--- database/migrations/2021_06_24_180126_create_media_table.php
+++ database/migrations/2021_06_24_180126_create_media_table.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 
 class CreateMediaTable extends Migration
 {
@@ -16,7 +16,7 @@ class CreateMediaTable extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->morphs('model');
+            $table->uuidMorphs('model');
             $table->string('collection_name');
             $table->string('name');
             $table->string('file_name');
